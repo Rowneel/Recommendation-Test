d09c9fe6883d8eb1e34a4aadc083808868bcff92
riticbenz123 <ritikbyanjankar@gmail.com>
2024-10-22 21:54:21 +0545

    imported csv file into the database and modified the code to get data from database, convert it to pandas dataframe and print api

--- Game_recommender/api/models.py
+++ Game_recommender/api/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+class Game(models.Model):
+    app_id = models.IntegerField(primary_key=True)
+    title = models.CharField(max_length=255)
+    date_release = models.DateField()
+    price_original = models.FloatField()
+
+    def __str__(self):
+        return self.title
+    
+class Recommendation(models.Model):
+    app_id = models.ForeignKey(Game, on_delete=models.CASCADE)
+    user_id = models.IntegerField()
+    hours = models.FloatField()
+
+    def __str__(self):
+        return f"User {self.user_id} - Game {self.app_id}"
+
+
--- Game_recommender/api/views.py
+++ Game_recommender/api/views.py
@@ -1,6 +1,9 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from django.contrib.staticfiles import finders
+from api.models import Game,Recommendation
+# from . import models
+from api.serializers import GameSerializer,RecommendationSerializer
 import numpy as np
 import pandas as pd
 
@@ -16,33 +19,51 @@ def reduce_memory(df):
 
 @api_view(['GET'])
 def getData(request):
-    # gettign data from csv file
-    games_path = finders.find('src/games.csv')
-    recommendations_path = finders.find('src/recommendations.csv')
-    games = reduce_memory(pd.read_csv(games_path,usecols=["app_id","title","date_release","price_original"]))
-    recommendations = reduce_memory(pd.read_csv(recommendations_path,usecols=["app_id","hours","user_id"], ))
     
-    #merging the recommendations with the games data
-    recommend_with_users = recommendations.merge(games,on='app_id')
+    
+    #get game and recommendation data from database after importing csv file into the database
+    game_data = Game.objects.all().values("app_id","title","date_release","price_original")
+    game_df = pd.DataFrame(game_data)
+    # print(game_df)
+    
+    recommendation_data = Recommendation.objects.all().values("app_id","hours","user_id")
+    recommendation_df = pd.DataFrame(recommendation_data)
+    # print(recommendation_df)
+    
+    #merging the dataframes
+    recommend_with_users = recommendation_df.merge(game_df,on='app_id')
+
+    # print(recommend_with_users)
     
     #finding the number of people that played the game
     no_of_ppl_played_df = recommend_with_users.groupby('app_id').count()['user_id'].reset_index()
     #replacing the user_id with the no_of_ppl for clarity
     no_of_ppl_played_df.rename(columns={'user_id':'no_of_ppl'},inplace=True)
     
+    
+    
     #finding the average time that the users have played the game
     hours_played_df = recommend_with_users.groupby('app_id')["hours"].mean().reset_index()
     #replacing the hours with mean_hours for clarity
     hours_played_df.rename(columns={'hours':'mean_hours'},inplace=True)
     
+    
     #based on hours played sorted out the most played games based on average hours played by users on games that have more than 10000 players
     most_played_game_df = no_of_ppl_played_df.merge(hours_played_df,on='app_id')
     popular_games_df = most_played_game_df[most_played_game_df['no_of_ppl'] >= 10000].sort_values("mean_hours",ascending=False).head(10)
     
-    
     #merging the popular_games_df with the games data to get the titles of the games
-    top_games_df = popular_games_df.merge(games,on="app_id")
+    top_games_df = popular_games_df.merge(game_df,on="app_id")
     
-    #DataFrame to Dictionary conversion to get every game as dectionary
+    
+    #DataFrame to Dictionary conversion to get every game as dictionary
     games_list = top_games_df.to_dict(orient='records')
     return Response(games_list)
+    
+    
+    
+    #for serializer.....
+    
+#     games = Game.objects.all().values()[:10]  # Fetch all games
+#     serializer = GameSerializer(games, many=True)  # Serialize the games data
+#     return Response(serializer.data)  # Return the serialized data as JSON
