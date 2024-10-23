6023bc7c4add4f7c8eb7ddca1f29aae5b01f4db7
riticbenz123 <ritikbyanjankar@gmail.com>
2024-10-23 18:55:53 +0545

    rollback to using csv

--- Game_recommender/api/models.py
+++ Game_recommender/api/models.py
@@ -1,21 +1,21 @@
 from django.db import models
 
 # Create your models here.
-class Game(models.Model):
-    app_id = models.IntegerField(primary_key=True)
-    title = models.CharField(max_length=255)
-    date_release = models.DateField()
-    price_original = models.FloatField()
+# class Game(models.Model):
+#     app_id = models.IntegerField(primary_key=True)
+#     title = models.CharField(max_length=255)
+#     date_release = models.DateField()
+#     price_original = models.FloatField()
 
-    def __str__(self):
-        return self.title
+#     def __str__(self):
+#         return self.title
     
-class Recommendation(models.Model):
-    app_id = models.ForeignKey(Game, on_delete=models.CASCADE)
-    user_id = models.IntegerField()
-    hours = models.FloatField()
+# class Recommendation(models.Model):
+#     app_id = models.ForeignKey(Game, on_delete=models.CASCADE)
+#     user_id = models.IntegerField()
+#     hours = models.FloatField()
 
-    def __str__(self):
-        return f"User {self.user_id} - Game {self.app_id}"
+#     def __str__(self):
+#         return f"User {self.user_id} - Game {self.app_id}"
 
 
--- Game_recommender/api/serializers.py
+++ Game_recommender/api/serializers.py
@@ -1,15 +1,15 @@
-from rest_framework import serializers
-from .models import Game,Recommendation
+# from rest_framework import serializers
+# # from .models import Game,Recommendation
 
-class GameSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Game
-        fields = ['app_id', 'title', 'date_release', 'price_original']
+# # class GameSerializer(serializers.ModelSerializer):
+# #     class Meta:
+# #         model = Game
+# #         fields = ['app_id', 'title', 'date_release', 'price_original']
         
-class RecommendationSerializer(serializers.ModelSerializer):
-    # Include the related Game info using GameSerializer
-    app_id = GameSerializer()
+# # class RecommendationSerializer(serializers.ModelSerializer):
+# #     # Include the related Game info using GameSerializer
+# #     app_id = GameSerializer()
 
-    class Meta:
-        model = Recommendation
-        fields = ['app_id', 'user_id', 'hours']
+# #     class Meta:
+# #         model = Recommendation
+# #         fields = ['app_id', 'user_id', 'hours']
